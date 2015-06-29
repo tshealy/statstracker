89a61177b25ac49f56e68a0a9070bf02e52a75ce
Tripp Shealy <tripp.shealy@gmail.com>
2015-06-29 18:52:14 -0400

    normal mode api complete (except charts)

--- statstracker/activities/models.py
+++ statstracker/activities/models.py
@@ -13,15 +13,13 @@ class Activities(models.Model):
     profile = models.ForeignKey(Profile)
     creation_date = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
-    # description = models.CharField(max_length=500, null=True)
 
-    #may need to show how many days we have done this
-    # def total_day(self):
-    #     return self.day_set.count()
 
 class Stats(models.Model):
     activities = models.ForeignKey(Activities)
     stat = models.IntegerField()
     date = models.DateField(default=datetime.date.today)
+
+
 
 
--- statstracker/activities/serializers.py
+++ statstracker/activities/serializers.py
@@ -2,16 +2,26 @@ __author__ = 'trippshealy'
 
 from rest_framework import serializers
 from .models import Activities, Stats
+from users.models import Profile
 from django.contrib.auth.models import User
 from rest_framework.reverse import reverse
 from rest_framework.fields import SerializerMethodField
 
 
 class ActivitiesSerializer(serializers.ModelSerializer):
+    profile = serializers.PrimaryKeyRelatedField(read_only=True)
+    creation_date = serializers.IntegerField
+    title = serializers.CharField(max_length=255)
+    stats = serializers.HyperlinkedIdentityField(view_name='stats-list')
+
     class Meta:
         model = Activities
 
+        fields = ('id', 'profile', 'title', 'profile', 'creation_date', 'stats')
+
 
 class StatsSerializer(serializers.ModelSerializer):
+    activities = serializers.PrimaryKeyRelatedField
+
     class Meta:
         model = Stats
