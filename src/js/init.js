3be4ee3589cd16081ff32e35336d21ebbd4bc8c2
jashelton <justin.shelton@yahoo.com>
2015-06-30 14:52:02 -0400

    routing, minor styling

---
+++ src/js/init.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var bulk = require('bulk-require');
+var router = require('./router');
+
+//Require all of our controllers
+bulk(__dirname, ['controllers/**/*.js']);
+
+// Start the router
+router.init();
