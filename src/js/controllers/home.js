6affd4360d15e3b7b882708899a6f35e83292436
jashelton <justin.shelton@yahoo.com>
2015-06-30 18:06:50 -0400

    sorting sorted, displaying activities list

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -14,16 +14,45 @@ router.route('', 'index', function () {
 
 $(function() {
 
+  var csrftoken = getCookie('csrftoken');
+
   $.ajax({
-    url: 'api/',
+    headers: { "X-CSRFToken": csrftoken },
+    url: '/api/activities/',
     method: 'GET',
     type: 'application/json'
   })
-  .done(function() {
-    alert( "success" );
+  .done(function(data) {
+    console.log(data.length);
+    for (var i = 0; i < data.length; i++) {
+      var title = $(data[i]).attr('title');
+      console.log(title);
+      var contentHtml = '<li class="activity-li">' + title + '<a href="#instances"><span class="add-instance">+</span></a></li>'
+      $('.activity-ul').append(contentHtml);
+  }
+    console.log('pass');
   })
   .fail(function() {
     alert( "error" );
   });
+
+function getCookie(name) {
+ var cookieValue = null;
+ if (document.cookie && document.cookie != '') {
+     var cookies = document.cookie.split(';');
+     for (var i = 0; i < cookies.length; i++) {
+         var cookie = $.trim(cookies[i]);
+         // Does this cookie string begin with the name we want?
+         if (cookie.substring(0, name.length + 1) == (name + '=')) {
+             cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+             break;
+         }
+     }
+ }
+ return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+console.log(csrftoken)
 
 });
--- src/js/controllers/new-activity-controller.js
+++ src/js/controllers/new-activity-controller.js
@@ -5,6 +5,7 @@ var _ = require('underscore');
 var views = require('views');
 var router = require('../router');
 
+
 router.route('new-activity', function () {
   var activityHTML = views['new-activity'];
 
@@ -23,5 +24,55 @@ $(function() {
     $(this).siblings('span').removeClass('focus-in');
   }
   });
+
+  var csrftoken = getCookie('csrftoken');
+
+  $('.login-form').submit(function(e) {
+    e.preventDefault();
+
+    var title = $('.activity-title').val();
+    console.log(title);
+
+    $.ajax({
+      headers: { "X-CSRFToken": csrftoken },
+      url: '/api/activities/',
+      method: 'POST',
+      data: ({
+        id: 50,
+        profile: 10,
+        title: title
+      }),
+    })
+    .done(function(data){
+      alert('posted');
+      console.log(data);
+    })
+    .fail(function() {
+      alert('didnt post');
+    });
+    window.location = '';
+  });
+
+
+
+
+  function getCookie(name) {
+   var cookieValue = null;
+   if (document.cookie && document.cookie != '') {
+       var cookies = document.cookie.split(';');
+       for (var i = 0; i < cookies.length; i++) {
+           var cookie = $.trim(cookies[i]);
+           // Does this cookie string begin with the name we want?
+           if (cookie.substring(0, name.length + 1) == (name + '=')) {
+               cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+               break;
+           }
+       }
+   }
+   return cookieValue;
+  }
+  var csrftoken = getCookie('csrftoken');
+
+
 
 });
